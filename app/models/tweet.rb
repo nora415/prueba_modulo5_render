217834850a6373f6067d9cf7181e430f5747c869
KONEKO <nora.maraboli.rivera@gmail.com>
2024-11-05 19:56:19 -0300

    primero commit con faker en seed.rb

---
+++ app/models/tweet.rb
@@ -0,0 +1,11 @@
+class Tweet < ApplicationRecord
+    include PgSearch::Model
+    pg_search_scope :search_full_text,
+    against: {  
+    UserName: 'A',
+    Description: 'B',
+    }
+   end
+
+
+
